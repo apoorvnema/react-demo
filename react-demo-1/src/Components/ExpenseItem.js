2c5fa67ad6f67754ce9ac4ee5068221c5d02abfc
apoorvnema38 <apoorvnema38@gmail.com>
2024-07-21 11:42:48 +0530

    [CHANGE]: ExpenseItem Component

--- react-demo-1/src/Components/ExpenseItem.js
+++ react-demo-1/src/Components/ExpenseItem.js
@@ -1,9 +1,10 @@
 function ExpenseItem(){
     return (
       <>
-        <p>Food - $5</p>
-        <p>Petrol - $20</p>
-        <p>Movie - $10</p>
+        <div>Date <p>15 August 2023</p></div>
+        <div>Title <p>Book</p></div>
+        <div>Amount <p>$10</p></div>
+        <div>Location <p>Delhi</p></div>
       </>
     )
   }
