8e6db4ed6e5978fb4a4d304909aeca9989c7f5c1
apoorvnema38 <apoorvnema38@gmail.com>
2024-07-21 11:35:58 +0530

    [NEW]: First Component

---
+++ react-demo-1/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ExpenseItem from "./Components/ExpenseItem";
+export default function App() {
+  return (
+    <>
+    <h1>Let's get Started</h1>
+    <ExpenseItem></ExpenseItem>
+    </>)
+}
---
+++ react-demo-1/src/Components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+      <>
+        <p>Food - $5</p>
+        <p>Petrol - $20</p>
+        <p>Movie - $10</p>
+      </>
+    )
+  }
+  
+  export default ExpenseItem;
