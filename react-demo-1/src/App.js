bd7793eb9f9f4c47778ac16ac95f1b1243741fbf
apoorvnema38 <apoorvnema38@gmail.com>
2024-07-21 12:06:32 +0530

    [UPDATE]: Added Props

--- react-demo-1/src/App.js
+++ react-demo-1/src/App.js
@@ -1,9 +1,35 @@
-import React from "react";
 import ExpenseItem from "./Components/ExpenseItem";
-export default function App() {
+
+function App() {
   return (
-    <>
-    <h1>Let's get Started</h1>
-    <ExpenseItem></ExpenseItem>
-    </>)
+    <div>
+      <h1>Let's get Started</h1>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        title="Insurance"
+        price="50"
+        location="Bangalore"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        title="Book"
+        price="20"
+        location="Delhi"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        title="Pen"
+        price="5"
+        location="Hyderabad"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        title="Laptop"
+        price="200"
+        location="Mumbai"
+      ></ExpenseItem>
+    </div>
+  );
 }
+
+export default App;
--- react-demo-1/src/Components/ExpenseItem.js
+++ react-demo-1/src/Components/ExpenseItem.js
@@ -1,20 +1,13 @@
-// Write yoour code at relevant places in the code below:
-
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2023, 7, 15).toISOString();
-  const expenseTitle = "Insurance";
-  const expenseAmount = 50;
-  const expenseLocation = 'Bangalore'
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate}</div>
-      <div className="expense-item__location">{expenseLocation}</div>
+      <div>{props.date.toISOString()}</div>
+      <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
       </div>
     </div>
   );
