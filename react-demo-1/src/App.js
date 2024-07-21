9370f60eefb6b63f85959a1a232d6178f37d6686
apoorvnema38 <apoorvnema38@gmail.com>
2024-07-21 14:01:29 +0530

    [UPDATE]: Date Formatting

--- react-demo-1/src/App.js
+++ react-demo-1/src/App.js
@@ -2,22 +2,21 @@ import ExpenseItem from "./Components/ExpenseItem";
 
 function App() {
   const expenses = [
-    { id: "1", date: new Date(2023, 7, 15), location:'Bangalore', title: "Insurance", price: 100 },
-    { id: "2", date: new Date(2023, 3, 25), location:'Delhi', title: "Book", price: 10 },
-    { id: "3", date: new Date(2023, 10, 11), location:'Hyderabad', title: "Pen", price: 1 },
-    { id: "4", date: new Date(2023, 1, 14), location:'Mumbai', title: "Laptop", price: 200 },
+    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
+    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
+    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
   ];
 
   return (
     <div>
-      <h1>Let's get Started</h1>
-      {expenses.map((expense, index) => {
+      <h2>Let's get Started</h2>
+      {expenses.map((expense) => {
         return (
           <ExpenseItem
             key={expense.id}
-            date={expense.date}
-            location={expense.location}
             title={expense.title}
+            date={expense.date}
             price={expense.price}
           ></ExpenseItem>
         );
--- react-demo-1/src/Components/ExpenseItem.js
+++ react-demo-1/src/Components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+  const month = months[props.date.getMonth()];
+  const year = props.date.getFullYear();
+  const day = String(props.date.getDate()).padStart(2, '0');
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
