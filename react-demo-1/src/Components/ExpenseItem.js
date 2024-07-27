61ff546beeec8c8fb7a090c611df4b7c21e257ec
apoorvnema <apoorvnema38@gmail.com>
2024-07-27 19:57:54 +0530

    [ADDED]: onClick Event

--- react-demo-1/src/Components/ExpenseItem.js
+++ react-demo-1/src/Components/ExpenseItem.js
@@ -3,13 +3,16 @@ import "./ExpenseItem.css";
 import Card from './Card'
 
 function ExpenseItem(props) {
+    function handleBtnClick() {
+        console.log("Button clicked");
+    }
     return (
         <Card className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__location">{props.location}</div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.price}</div>
+                <button onClick={handleBtnClick} value="Change Title">Change Title</button>
             </div>
         </Card>
     );
