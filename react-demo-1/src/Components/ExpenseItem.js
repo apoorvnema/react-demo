51c68c76d700cd0a42735404906516e7290b5805
apoorvnema <apoorvnema38@gmail.com>
2024-07-27 20:08:53 +0530

    [ADDED]: useState Hook

--- react-demo-1/src/Components/ExpenseItem.js
+++ react-demo-1/src/Components/ExpenseItem.js
@@ -2,20 +2,23 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from './Card'
 
-function ExpenseItem(props) {
-    function handleBtnClick() {
-        console.log("Button clicked");
+const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.title);
+
+    function buttonClickHandler() {
+        setTitle("New Title");
     }
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.price}</div>
-                <button onClick={handleBtnClick} value="Change Title">Change Title</button>
             </div>
+            <button onClick={buttonClickHandler}>Change Title</button>
         </Card>
     );
-}
+};
 
 export default ExpenseItem;
