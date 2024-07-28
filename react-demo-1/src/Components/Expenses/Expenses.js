b35b0938fbe7d36e27b6167eacc1951d027b9310
apoorvnema <apoorvnema38@gmail.com>
2024-07-28 15:27:50 +0530

    [UPDATE]: Conditional Rendering in Filter

--- react-demo-1/src/Components/Expenses/Expenses.js
+++ react-demo-1/src/Components/Expenses/Expenses.js
@@ -1,3 +1,5 @@
+// Write your code at relevant places in the code below:
+
 import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
@@ -15,22 +17,32 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
+    let expensesContent = <p>No expenses found</p>;
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => {
+            return (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    date={expense.date}
+                    price={expense.price}
+                />
+            );
+        });
+    }
+    if (filteredExpenses.length === 1) {
+        expensesContent.push(
+            <p>Only one expense found. Please add more</p>
+        );
+    }
+
     return (
         <Card className="expenses">
             <ExpensesFilter
                 selected={filteredYear}
                 onChangeFilter={changeFilterHandler}
             />
-            {filteredExpenses.map((expense) => {
-                return (
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        date={expense.date}
-                        price={expense.price}
-                    />
-                );
-            })}
+            {expensesContent}
         </Card>
     );
 };
