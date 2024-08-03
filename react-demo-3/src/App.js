29bfd03c142e224fd16d71258c4220bdb400d086
apoorvnema <apoorvnema38@gmail.com>
2024-08-03 19:03:32 +0530

    [UPDATE]: Managing Users List

--- react-demo-3/src/App.js
+++ react-demo-3/src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./components/Users/AddUser";
 import UsersList from "./components/Users/UsersList";
 
 function App() {
-  const users = [];
+  const [users, setUsers] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsers((prev) => {
+      return [...prev, { 'name': uName, 'age': uAge, 'id': Math.random().toString() }]
+    })
+  }
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={users}/>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={users} />
     </div>
   );
 }
--- react-demo-3/src/components/Users/UsersList.js
+++ react-demo-3/src/components/Users/UsersList.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import Card from '../UI/Card';
-import './UsersList.css';
+import React from "react";
+import Card from "../UI/Card";
+import "./UsersList.css";
 
 const UsersList = (props) => {
     return (
         <Card className="users">
             <ul>
-                {props.users.map(user => (
-                    <li>{user.name} ({user.age} years old)</li>
-                ))}
+                {props.users.map((user) => {
+                    return (
+                        <li key={user.id}>
+                            {user.name} ({user.age} years old)
+                        </li>
+                    );
+                })}
             </ul>
         </Card>
     );
