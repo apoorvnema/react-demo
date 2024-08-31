4e3f585f4e01c5a74b5e9c9fdd6c1a84dc7f1a3b
apoorvnema <apoorvnema38@gmail.com>
2024-08-31 18:54:06 +0530

    [UPDATE]: New Movie Project with Loader

---
+++ react-demo-5/src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMoviesHandler = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://swapi.dev/api/films/')
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    }
+    catch (error) {
+      console.log(error)
+    }
+    finally{
+      setIsLoading(false);
+    }
+  }
+
+return (
+  <React.Fragment>
+
+    <section>
+      <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+    </section>
+    <section>
+      {!isLoading && <MoviesList movies={movies} />}
+      {!isLoading && movies.length === 0 && <p>No movies found</p>}
+      {isLoading && <p>Loading...</p>}
+    </section>
+  </React.Fragment>
+);
+}
+
+export default App;
