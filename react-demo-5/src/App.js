fb7c2c14728002ce6b80254cf606733094d0876c
apoorvnema <apoorvnema38@gmail.com>
2024-08-31 19:45:23 +0530

    [UPDATE]: Fetch movie on Page load with Usecallback

--- react-demo-5/src/App.js
+++ react-demo-5/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -10,7 +10,7 @@ function App() {
   const retryTimer = useRef(null);
   const [retrying, setRetrying] = useState(false);
 
-  const fetchMoviesHandler = async () => {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
@@ -36,7 +36,11 @@ function App() {
     } finally {
       setIsLoading(false);
     }
-  };
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   useEffect(() => {
     if (retrying) {
