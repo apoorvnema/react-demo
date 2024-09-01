7dd1b41fa14dbc7218f453f0f960be4c6c8fee43
apoorvnema <apoorvnema38@gmail.com>
2024-09-01 12:52:10 +0530

    [UPDATE]: Authentication App

---
+++ react-demo-6/src/components/Auth/AuthForm.js
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await new Promise((resolve, reject) => setTimeout(() => reject(new Error('Signup failed')), 2000));
+    } catch (err) {
+      alert(err.message);
+    }
+
+    setIsLoading(false);
+  };
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input type='password' id='password' required />
+        </div>
+        <div className={classes.actions}>
+          {!isLoading && (
+            <button type='submit'>
+              {isLogin ? 'Login' : 'Sign Up'}
+            </button>
+          )}
+          {isLoading && <p>Sending request...</p>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
