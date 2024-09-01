a1ca2dc8532cc91eea651d90da985f8857ffe751
apoorvnema <apoorvnema38@gmail.com>
2024-09-01 15:31:24 +0530

    [UPDATE]: Logout Functionality

--- react-demo-6/src/components/Layout/MainNavigation.js
+++ react-demo-6/src/components/Layout/MainNavigation.js
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 import classes from './MainNavigation.module.css';
 import { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
+import { useHistory } from 'react-router-dom';
 
 const MainNavigation = () => {
   const authCtx = useContext(AuthContext);
+  const history = useHistory();
   const isLoggedIn = authCtx.isLoggedIn;
+
+  const logoutHandler = () => {
+    authCtx.logout();
+    history.replace('/auth');
+  };
 
   return (
     <header className={classes.header}>
@@ -27,7 +34,7 @@ const MainNavigation = () => {
           )}
           {isLoggedIn && (
             <li>
-              <button onClick={authCtx.logout}>Logout</button>
+              <button onClick={logoutHandler}>Logout</button>
             </li>
           )}
         </ul>
